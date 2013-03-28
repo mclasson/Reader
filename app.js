2444f129157c934122043ee9e52cac87d4ffe13d
Marcus Classon <marcus.g.classon@gmail.com>
2013-03-28 12:48:37 +0100

    added sql

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  , hike = require('./routes/hike');
+  , hike = require('./routes/hike')
+  , mysql = require('mysql')
+  , async = require('async');
 
 var app = express();
 
@@ -24,8 +26,23 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+    console.log('Using development settings.');
+    app.set('connection', mysql.createConnection({
+        host: '',
+        user: '',
+        port: '',
+        password: ''}));
+    app.use(express.errorHandler());
+});
+
+app.configure('production', function() {
+    console.log('Using production settings.');
+    app.set('connection', mysql.createConnection({
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT}));
 });
 
 app.get('/', routes.index);
@@ -35,4 +52,43 @@ app.post('/add_hike', hike.add_hike);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+var client = app.get('connection');
+async.series([
+    function connect(callback) {
+        client.connect(callback);
+    },
+    function clear(callback) {
+        client.query('DROP DATABASE IF EXISTS mynode_db', callback);
+    },
+    function create_db(callback) {
+        client.query('CREATE DATABASE mynode_db', callback);
+    },
+    function use_db(callback) {
+        client.query('USE mynode_db', callback);
+    },
+    function create_table(callback) {
+        client.query('CREATE TABLE HIKES (' +
+            'ID VARCHAR(40), ' +
+            'HIKE_DATE DATE, ' +
+            'NAME VARCHAR(40), ' +
+            'DISTANCE VARCHAR(40), ' +
+            'LOCATION VARCHAR(40), ' +
+            'WEATHER VARCHAR(40), ' +
+            'PRIMARY KEY(ID))', callback);
+    },
+    function insert_default(callback) {
+        var hike = {HIKE_DATE: new Date(), NAME: 'Hazard Stevens',
+            LOCATION: 'Mt Rainier', DISTANCE: '4,027m vertical', WEATHER:'Bad'};
+        client.query('INSERT INTO HIKES set ?', hike, callback);
+    }
+], function (err, results) {
+    if (err) {
+        console.log('Exception initializing database.');
+        throw err;
+    } else {
+        console.log('Database initialization complete.');
+        init();
+    }
 });
